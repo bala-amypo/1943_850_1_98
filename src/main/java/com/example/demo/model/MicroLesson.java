af3d94a984f00ff142d569d00072844dbae63045
bala-amypo <bala@amypo.in>
2025-12-21 06:08:54 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/MicroLesson.java
@@ -0,0 +1,32 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MicroLesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Course course;
+
+    private String title;
+
+    private Integer durationMinutes;
+
+    private String contentType;
+
+    private String difficulty;
+
+    private String tags;
+
+    private LocalDate publishDate;
+}
