5461e41b8524a8f4961d295fee32033a77cd9bd2
bala-amypo <bala@amypo.in>
2025-12-26 13:09:45 +0000

    User commit

--- src/main/java/com/example/demo/service/RecommendationService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.Recommendation;
-
-public interface RecommendationService {
-
-    Recommendation getLatestRecommendation(Long userId);
-}
