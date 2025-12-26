99e6a3a3f97a453a891c653a74be606a28ffe758
bala-amypo <bala@amypo.in>
2025-12-26 12:43:10 +0000

    User commit

--- src/main/java/com/example/demo/dto/RecommendationRequest.java
+++ src/main/java/com/example/demo/dto/RecommendationRequest.java
@@ -1,9 +1,33 @@
 package com.example.demo.dto;
 
-import lombok.Data;
+import java.util.List;
 
-@Data
 public class RecommendationRequest {
 
     private Long userId;
+    private List<Long> completedLessonIds;
+
+    public RecommendationRequest() {
+    }
+
+    public RecommendationRequest(Long userId, List<Long> completedLessonIds) {
+        this.userId = userId;
+        this.completedLessonIds = completedLessonIds;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public List<Long> getCompletedLessonIds() {
+        return completedLessonIds;
+    }
+
+    public void setCompletedLessonIds(List<Long> completedLessonIds) {
+        this.completedLessonIds = completedLessonIds;
+    }
 }
