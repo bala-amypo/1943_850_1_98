6cbfa6721ddd4dcdc4ed501d9538e6ebba954164
bala-amypo <bala@amypo.in>
2025-12-26 09:28:09 +0000

    User commit

--- src/main/java/com/example/demo/repository/ProgressRepository.java
+++ src/main/java/com/example/demo/repository/ProgressRepository.java
@@ -1,9 +1,18 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.Progress;
+import com.example.demo.model.Progress;
+import com.example.demo.model.User;
+import com.example.demo.model.MicroLesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProgressRepository extends JpaRepository<Progress, Long> {
+
+    Optional<Progress> findByUserAndMicroLesson(User user, MicroLesson microLesson);
+
+    List<Progress> findByUser(User user);
 }
