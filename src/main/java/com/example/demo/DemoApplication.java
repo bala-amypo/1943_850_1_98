7a266fa8b881ad798da0cacd73346a432a93fc8b
bala-amypo <bala@amypo.in>
2025-12-26 09:46:10 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,3 +11,17 @@ public class DemoApplication {
 	}
 
 }
+
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -16,5 +16,5 @@ public class SwaggerConfig {
                 .servers(List.of(
                         new Server().url("https://9244.408procr.amypo.ai/")
                 ));
-        }
+        } 
 } 
